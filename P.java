d7ae4a2d8dec2ff7517c718935b70f2d1d9b84af
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-05 14:50:18 +0530

    Add files via upload

---
+++ P.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class P{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int n=sc.nextInt();
+            for(int j=1;j<n;j++)
+            {
+                if(n%j==0)
+                System.out.println(j+" ");
+            }
+            System.out.println();    
+        }    
+    
+    }
+
+}
