4f8ade460674cb90641dfb6e1d26a52e7f286744
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-05 14:33:18 +0530

    Add files via upload

---
+++ C.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class C{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        {
+            int n=sc.nextInt();
+            if((n&7)==0)
+            {
+                System.out.println("Multiple of 8");
+            }else{
+                System.out.println("Not Multiple of 8");
+                
+            }
+        }
+    }    
+}
