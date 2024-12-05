12880d5d6a380500bff1adcc2222d3afcf465179
qureshi2714 <msmuskanshaik65@gmail.com>
2024-12-05 16:05:59 +0530

    Add files via upload

---
+++ Whileloop.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Whileloop{
+    public static void main(String args[]){
+        int n=5;
+        System.out.println(n++);
+        int a=4;
+        System.out.println(++a);
+    }  
+    
+}
